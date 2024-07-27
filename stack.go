0000e578678b9015d3bf2671f673b04500856e22
Mac21 <russ.tolle@gmail.com>
2024-07-27 10:49:47 -0600

    Simplify stack ds

--- stack.go
+++ stack.go
@@ -2,26 +2,22 @@ package gods
 
 type Stack[T any] []T
 
-func (s Stack[T]) Push(v T) Stack[T] {
-	return append(s, v)
+func (s *Stack[T]) Push(v T) {
+	*s = append(*s, v)
 }
 
-func (s Stack[T]) Pop() (Stack[T], T, bool) {
-	if len(s) < 1 {
+func (s *Stack[T]) Pop() (T, bool) {
+	if len(*s) < 1 {
         var zv T
-		return s, zv, true
+		return zv, true
 	}
 
-    res := s[len(s)-1]
-	s = s[:len(s)-1]
+    res := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
 
-	return s, res, false
+	return res, false
 }
 
 func (s Stack[T]) Len() int {
-	if s == nil {
-		return 0
-	}
-
 	return len(s)
 }
--- stack_test.go
+++ stack_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestPushOne(t *testing.T) {
     st := Stack[int]{}
-    st = st.Push(7)
+    st.Push(7)
 
     if st.Len() != 1 {
         t.Fatalf("Expected stack len to be 1 got %d", st.Len())
@@ -15,19 +15,20 @@ func TestPushOne(t *testing.T) {
 
 func TestPop(t *testing.T) {
     st := Stack[int]{}
-    st = st.Push(7).Push(8)
+    st.Push(7)
+    st.Push(8)
 
-    st, top, _ := st.Pop()
+    top, _ := st.Pop()
     if st.Len() != 1 && top != 8 {
         t.Fatalf("Expected Pop 8 from top of stack got %d", top)
     }
 
-    st, top, _ = st.Pop()
+    top, _ = st.Pop()
     if st.Len() != 0 && top != 7 {
         t.Fatalf("Expected Pop 7 from top of stack got %d", top)
     }
 
-    _, top, empty := st.Pop()
+    _, empty := st.Pop()
     if !empty {
         t.Fatalf("Expected third pop to be empty got %d", top)
     }
