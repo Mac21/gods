e7a6ed3f9fa5f3b826795bc3e16c22e1727caff8
Mac21 <russ.tolle@gmail.com>
2024-07-27 21:49:27 -0600

    Add tests for exists

--- priority_queue_test.go
+++ priority_queue_test.go
@@ -28,7 +28,7 @@ func TestIntMinPQ(t *testing.T) {
         }
 
         if res != expectedAnswer {
-            t.Fatalf("pq.Pop() result doesn't match expected %v got %v", res, expectedAnswer)
+            t.Fatalf("pq.Pop() result doesn't match expected %v got %v", expectedAnswer, res)
         }
 
         expectedLen := len(answer[i:])
@@ -106,12 +106,130 @@ func TestStructMinPQ(t *testing.T) {
         }
 
         if res != expectedAnswer {
-            t.Fatalf("pq.Pop() result doesn't match expected %v got %v", res, expectedAnswer)
+            t.Fatalf("pq.Pop() result doesn't match expected %v got %v", expectedAnswer, res)
         }
 
         expectedLen := len(answer[i:])
         if pq.Len() != expectedLen {
             t.Fatalf("priority queue Len() doesn't match expected %v got %v", expectedLen, pq.Len())
+        }
+
+    }
+}
+
+func TestUpdatePriority(t *testing.T) {
+    type CustomValue struct {
+        Number int
+        Weight int
+    }
+    input := []CustomValue{
+        {
+            Number: 0,
+            Weight: 7,
+        },
+        {
+            Number: 1,
+            Weight: 6,
+        },
+        {
+            Number: 2,
+            Weight: 5,
+        },
+        {
+            Number: 3,
+            Weight: 1,
+        },
+        {
+            Number: 4,
+            Weight: 2,
+        },
+    }
+    pq := NewPriorityQueue[CustomValue]()
+    for _, cv := range input {
+        pq.Push(cv, cv.Weight)
+    }
+
+    pq.Update(input[3], 9)
+
+    i := 0
+    answer := []CustomValue{
+        {
+            Number: 4,
+            Weight: 2,
+        },
+        {
+            Number: 2,
+            Weight: 5,
+        },
+        {
+            Number: 1,
+            Weight: 6,
+        },
+        {
+            Number: 0,
+            Weight: 7,
+        },
+        {
+            Number: 3,
+            Weight: 1,
+        },
+
+    }
+    for pq.Len() > 0 {
+        res, err := pq.Pop()
+        expectedAnswer := answer[i]
+        i++
+        if err != nil {
+            t.Fatal(err)
+        }
+
+        if res != expectedAnswer {
+            t.Fatalf("pq.Pop() result doesn't match expected %v got %v", expectedAnswer, res)
+        }
+
+        expectedLen := len(answer[i:])
+        if pq.Len() != expectedLen {
+            t.Fatalf("priority queue Len() doesn't match expected %v got %v", expectedLen, pq.Len())
+        }
+
+    }
+}
+
+func TestCustomValuesExist(t *testing.T) {
+    type CustomValue struct {
+        Number int
+        Weight int
+    }
+    input := []CustomValue{
+        {
+            Number: 0,
+            Weight: 7,
+        },
+        {
+            Number: 1,
+            Weight: 6,
+        },
+        {
+            Number: 2,
+            Weight: 5,
+        },
+        {
+            Number: 3,
+            Weight: 1,
+        },
+        {
+            Number: 4,
+            Weight: 2,
+        },
+    }
+    pq := NewPriorityQueue[CustomValue]()
+    for _, cv := range input {
+        pq.Push(cv, cv.Weight)
+    }
+
+    for i := 0; i < pq.Len(); i++ {
+        if exists := pq.Exists(input[i]); !exists {
+            t.Fatalf("pq.Exist() result doesn't match expected %v got %v", exists, true)
         }
 
     }
