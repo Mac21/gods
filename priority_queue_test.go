82622cf01f60e77df3d68809c9272f9b131b71d8
Mac21 <russ.tolle@gmail.com>
2024-07-28 13:08:06 -0600

    Add tests for min and max queue types

--- priority_queue_test.go
+++ priority_queue_test.go
@@ -39,6 +39,118 @@ func TestIntMinPQ(t *testing.T) {
 	}
 }
 
+func TestIntMaxPQ(t *testing.T) {
+	input := map[int]int{
+		0: 5,
+		1: 2,
+		2: 3,
+		3: 4,
+		4: 1,
+	}
+	pq := NewMaxPriorityQueue[int, int]()
+	for v, p := range input {
+		pq.Push(v, p)
+	}
+
+	i := 0
+	answer := []int{0, 3, 2, 1, 4}
+	for pq.Len() > 0 {
+		res, _, ok := pq.Pop()
+		expectedAnswer := answer[i]
+		i++
+		if !ok {
+			t.Fatal("pq.Pop() popped an empty queue")
+		}
+
+		if res != expectedAnswer {
+			t.Fatalf("pq.Pop() result doesn't match expected %v got %v", expectedAnswer, res)
+		}
+
+		expectedLen := len(answer[i:])
+		if pq.Len() != expectedLen {
+			t.Fatalf("priority queue Len() doesn't match expected %v got %v", expectedLen, pq.Len())
+		}
+
+	}
+}
+
+func TestStructMaxPQ(t *testing.T) {
+	type CustomValue struct {
+		Number int
+		Weight int
+	}
+	input := []CustomValue{
+		{
+			Number: 0,
+			Weight: 7,
+		},
+
+		{
+			Number: 1,
+			Weight: 6,
+		},
+		{
+			Number: 2,
+			Weight: 5,
+		},
+		{
+			Number: 3,
+			Weight: 1,
+		},
+		{
+			Number: 4,
+			Weight: 2,
+		},
+	}
+	pq := NewMaxPriorityQueue[CustomValue, int]()
+	for _, cv := range input {
+		pq.Push(cv, cv.Weight)
+	}
+
+	i := 0
+	answer := []CustomValue{
+		{
+			Number: 0,
+			Weight: 7,
+		},
+
+		{
+			Number: 1,
+			Weight: 6,
+		},
+		{
+			Number: 2,
+			Weight: 5,
+		},
+		{
+			Number: 4,
+			Weight: 2,
+		},
+		{
+			Number: 3,
+			Weight: 1,
+		},
+	}
+	for pq.Len() > 0 {
+		res, _, ok := pq.Pop()
+		expectedAnswer := answer[i]
+		i++
+		if !ok {
+			t.Fatal("pq.Pop() popped an empty queue")
+		}
+
+		if res != expectedAnswer {
+			t.Fatalf("pq.Pop() result doesn't match expected %v got %v", expectedAnswer, res)
+		}
+
+		expectedLen := len(answer[i:])
+		if pq.Len() != expectedLen {
+			t.Fatalf("priority queue Len() doesn't match expected %v got %v", expectedLen, pq.Len())
+		}
+
+	}
+}
+
 func TestStructMinPQ(t *testing.T) {
 	type CustomValue struct {
 		Number int
